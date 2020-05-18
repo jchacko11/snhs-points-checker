204522430ac42546c4b0708bbb7a8c6466ba7a40
jchacko11 <jonathan.j.chacko@gmail.com>
2020-05-18 17:36:35 -0500

    Add function to get specific data about a member, such as event attendance or meeting attendance

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import gspread
+
 client = gspread.service_account(filename='snhs-points-checker-2c4f229c7577.json')
+db = client.open("Member Database 2020-21")
 
 
 def get_member(id):
     # Get master workbook
-    sheet = client.open("Member Database 2020-21").worksheet("Master")
+    sheet = db.worksheet("Master")
 
     # Extract and filter by id
     list_of_hashes = sheet.get_all_records()
@@ -13,5 +15,21 @@ def get_member(id):
     return member
 
 
-var = get_member(203046)["Full Name"]
-print(var)
+def get_specifics(id, sheet_name='Meeting Attendance'):
+    sheet = db.worksheet(sheet_name)
+    all_values = sheet.get_all_values()
+
+    # transpose array
+    transposed = list(map(list, zip(*all_values)))
+
+    attended_meetings = []
+
+    # look for items where member participated
+    for item in transposed:
+        if str(id) in item:
+            attended_meetings.append(item[0])
+
+    return attended_meetings
+
+
+print(get_specifics(342899, "Events"))
