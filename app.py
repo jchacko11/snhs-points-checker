41ee1d8bdb40ded100c48e20c42956c6082e37ec
jchacko11 <jonathan.j.chacko@gmail.com>
2020-05-18 17:48:03 -0500

    Add flask app

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
 import gspread
+from flask import Flask, render_template
 
 client = gspread.service_account(filename='snhs-points-checker-2c4f229c7577.json')
 db = client.open("Member Database 2020-21")
 
+app = Flask(__name__)
 
-def get_member(id):
+
+def get_member(member_id):
     # Get master workbook
     sheet = db.worksheet("Master")
 
     # Extract and filter by id
     list_of_hashes = sheet.get_all_records()
-    member = list(filter(lambda i: i["Student ID"] == int(id), list_of_hashes))[0]
+    member = list(filter(lambda i: i["Student ID"] == int(member_id), list_of_hashes))[0]
 
     return member
 
 
-def get_specifics(id, sheet_name='Meeting Attendance'):
+def get_specifics(member_id, sheet_name='Meeting Attendance'):
     sheet = db.worksheet(sheet_name)
     all_values = sheet.get_all_values()
 
@@ -26,10 +29,16 @@ def get_specifics(id, sheet_name='Meeting Attendance'):
 
     # look for items where member participated
     for item in transposed:
-        if str(id) in item:
+        if str(member_id) in item:
             attended_meetings.append(item[0])
 
     return attended_meetings
 
 
-print(get_specifics(342899, "Events"))
+@app.route('/')
+def index():
+    return 'Hello, World!'
+
+
+if __name__ == '__main__':
+    app.run()
