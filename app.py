53e7d246c48c28e1c96554c6e2d7654fb641d16c
jchacko11 <jonathan.j.chacko@gmail.com>
2020-05-18 19:45:15 -0500

    Adding a frontend to access points

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import gspread
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 client = gspread.service_account(filename='snhs-points-checker-2c4f229c7577.json')
 db = client.open("Member Database 2020-21")
@@ -13,9 +13,11 @@ def get_member(member_id):
 
     # Extract and filter by id
     list_of_hashes = sheet.get_all_records()
-    member = list(filter(lambda i: i["Student ID"] == int(member_id), list_of_hashes))[0]
-
-    return member
+    try:
+        member = list(filter(lambda i: i["Student ID"] == int(member_id), list_of_hashes))[0]
+        return member
+    except:
+        return False
 
 
 def get_specifics(member_id, sheet_name='Meeting Attendance'):
@@ -26,19 +28,47 @@ def get_specifics(member_id, sheet_name='Meeting Attendance'):
     transposed = list(map(list, zip(*all_values)))
 
     attended_meetings = []
+    attended_meetings_weight = []
+    modified_meetings = []
 
     # look for items where member participated
     for item in transposed:
         if str(member_id) in item:
             attended_meetings.append(item[0])
+            attended_meetings_weight.append(item[1])
 
-    return attended_meetings
+    for i in range(len(attended_meetings)):
+        if attended_meetings_weight and attended_meetings_weight != 10:
+            modified_meetings.append(attended_meetings[i] + " (" + attended_meetings_weight[i] + " points)")
+        else:
+            modified_meetings.append(attended_meetings[i])
+
+        print(modified_meetings)
+
+    return modified_meetings
 
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    return render_template("index.html")
+
+
+@app.route('/member')
+def member_data():
+    try:
+        member_id = int(request.args["member_id"])
+    except:
+        return render_template("index.html", valid=False)
+
+    member = get_member(member_id)
+
+    member_details = get_specifics(member_id, sheet_name='Meeting Attendance') + get_specifics(member_id, sheet_name='Events')
+
+    if member is not False:
+        return render_template("index.html", member=member, member_details=member_details, valid=True)
+    else:
+        return render_template("index.html", valid=False)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
---
+++ static/script.js
@@ -0,0 +1,39 @@
+// Restricts input for the set of matched elements to the given inputFilter function.
+(function ($) {
+    $.fn.inputFilter = function (inputFilter) {
+        return this.on("input keydown keyup mousedown mouseup select contextmenu drop", function () {
+            if (inputFilter(this.value)) {
+                this.oldValue = this.value;
+                this.oldSelectionStart = this.selectionStart;
+                this.oldSelectionEnd = this.selectionEnd;
+            } else if (this.hasOwnProperty("oldValue")) {
+                this.value = this.oldValue;
+                this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+            } else {
+                this.value = "";
+            }
+        });
+    };
+}(jQuery));
+
+
+$(document).ready(function () {
+    $("#studentId").inputFilter(function (value) {
+        return /^\d*$/.test(value);    // Allow digits only, using a RegExp
+    });
+
+    // make sure 6 digits before allowing submit
+    $("#studentId").on("change load keyup paste", function () {
+        var value = $(this).val()
+        console.log(value)
+        if (value.length == 6) {
+            $(search).attr("disabled", false)
+        } else {
+            $(search).attr("disabled", true)
+        }
+
+    })
+
+
+});
+
